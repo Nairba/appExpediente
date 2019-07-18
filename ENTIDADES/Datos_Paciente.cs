125131d11b4386ccb50138e7089624e72f51119a
Nairba <adrianmoova@gmail.com>
2019-07-18 17:21:17 -0600

    entidades

---
+++ ENTIDADES/Datos_Paciente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTIDADES
+{
+    public class Datos_Paciente
+    {
+        public int idDatosP { get; set; }
+        public DateTime fhechaNacimiento { get; set; }
+        public string tipoSangre { get; set; }
+        public string residiencia { get; set; }
+        public string telefono { get; set; }
+        public string cantactoEmergencia { get; set; }
+        public string parentesco { get; set; }
+    }
+}
