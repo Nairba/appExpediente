89cadb7be4660ebc874ec6197ea3dea57042d799
Nairba <adrianmoova@gmail.com>
2019-07-19 18:33:44 -0600

    Nuevaos cambios

---
+++ ENTIDADES/Cita.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTIDADES
+{
+    public class Cita
+    {
+        public int id { get; set; }
+        public DateTime hora { get; set; }
+        public DateTime fechaCita { get; set; }
+        public Paciente_Paciente_Asociado  paciente { get; set; }
+        public string descripcion { get; set; }
+        public Consulta consulta { get; set; }
+        public string estado { get; set; }
+        public string observaciones { get; set; }
+        public Horario horario { get; set; }
+
+        public Cita()
+        {
+            paciente = new  Paciente_Paciente_Asociado();
+            consulta = new Consulta();
+            horario = new Horario();
+        }
+    }
+}
---
+++ ENTIDADES/Expediente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTIDADES
+{
+   public class Expediente
+    {
+        public int id { get; set; }
+        public DateTime fecha { get; set; }
+        public string esttado { get; set; }
+        public Paciente_Paciente_Asociado paciante { get; set; }
+        public Usuario medico { get; set; }
+
+        public Expediente()
+        {
+            paciante = new Paciente_Paciente_Asociado();
+            medico =new Usuario();
+        }
+    }
+}
